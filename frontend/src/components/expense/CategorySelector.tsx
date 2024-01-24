608643b08a37ec210158c3fb6ecc29474d210d21
ThomNeuenschwander <130864240+ThomNeuenschwander@users.noreply.github.com>
2024-01-24 17:54:05 -0300

    code refatoration

--- frontend/src/components/expense/CategorySelector.tsx
+++ frontend/src/components/expense/CategorySelector.tsx
@@ -1,77 +1,88 @@
-import React, { useState, useEffect, ChangeEvent } from 'react';
-import { Category } from '../../resources/expense/expense.resource';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect, ChangeEvent } from "react"
+import { Category } from "../../resources/expense/expense.resource"
 
 interface CategorySelectorProps {
-  availableCategories: Category[] | null | undefined
-  onCategoriesChange: (categories: string[]) => void
-  style?: string
+   availableCategories: Category[] | null | undefined
+   onCategoriesChange: (categories: string[]) => void
+   style?: string
 }
 
-const CategorySelector: React.FC<CategorySelectorProps> = ({ availableCategories, onCategoriesChange, style }) => {
-  const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
-  const [newCategory, setNewCategory] = useState<string>("");
+const CategorySelector: React.FC<CategorySelectorProps> = ({
+   availableCategories,
+   onCategoriesChange,
+   style,
+}) => {
+   const [selectedCategories, setSelectedCategories] = useState<string[]>([])
+   const [newCategory, setNewCategory] = useState<string>("")
 
-  const handleCategoryChange = (event: ChangeEvent<HTMLSelectElement>) => {
-    setNewCategory(event.target.value);
-  };
+   const handleCategoryChange = (event: ChangeEvent<HTMLSelectElement>) => {
+      setNewCategory(event.target.value)
+   }
 
-  const handleAddCategory = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault()
-    if (newCategory.trim() !== "" && !selectedCategories.includes(newCategory)) {
-      setSelectedCategories([...selectedCategories, newCategory]);
-      setNewCategory('');
-    }
-  };
+   const handleAddCategory = (e: React.MouseEvent<HTMLButtonElement>) => {
+      e.preventDefault()
+      if (
+         newCategory.trim() !== "" &&
+         !selectedCategories.includes(newCategory)
+      ) {
+         setSelectedCategories([...selectedCategories, newCategory])
+         setNewCategory("")
+      }
+   }
 
-  const handleRemoveCategory = (category: string) => {
-    const updatedCategories = selectedCategories.filter((c) => c !== category);
-    setSelectedCategories(updatedCategories);
-  };
+   const handleRemoveCategory = (category: string) => {
+      const updatedCategories = selectedCategories.filter((c) => c !== category)
+      setSelectedCategories(updatedCategories)
+   }
 
-  useEffect(() => {
-    onCategoriesChange(selectedCategories);
-  }, [selectedCategories, onCategoriesChange]);
+   useEffect(() => {
+      onCategoriesChange(selectedCategories)
+   }, [selectedCategories])
 
-  return (
-    <div>
-      <div className="flex items-center">
-        <select
-          onChange={handleCategoryChange}
-          value={newCategory}
-          className={`border px-3 py-2 rounded-l-md focus:outline-none focus:border-blue-500 ${style}`}
-        >
-          <option value="" disabled>Select a category</option>
-          {availableCategories && availableCategories.map((category) => (
-            <option key={category.id} value={category.name}>
-              {category.name}
-            </option>
-          ))}
-        </select>
-        <button
-          onClick={handleAddCategory}
-          className="px-4 py-2 bg-blue-500 text-white rounded-r-md hover:bg-blue-700 focus:outline-none"
-        >
-          Add
-        </button>
-      </div>
-      <div className="mt-2 flex flex-wrap">
-        {selectedCategories.map((category) => (
-          <span
-            key={category}
-            className="bg-blue-500 text-white px-2 py-1 rounded-full mr-2 mt-2 flex items-center"
-          >
-            {category}
-            <button
-              onClick={() => handleRemoveCategory(category)}
-              className="ml-3 mr-1 focus:outline-none text-lg"
+   return (
+      <div>
+         <div className="flex items-center">
+            <select
+               onChange={handleCategoryChange}
+               value={newCategory}
+               className={`border px-3 py-2 rounded-l-md focus:outline-none focus:border-blue-500 ${style}`}
             >
-              x
+               <option value="" disabled>
+                  Select a category
+               </option>
+               {availableCategories &&
+                  availableCategories.map((category) => (
+                     <option key={category.id} value={category.name}>
+                        {category.name}
+                     </option>
+                  ))}
+            </select>
+            <button
+               onClick={handleAddCategory}
+               className="px-4 py-2 bg-blue-500 text-white rounded-r-md hover:bg-blue-700 focus:outline-none"
+            >
+               Add
             </button>
-          </span>
-        ))}
+         </div>
+         <div className="mt-2 flex flex-wrap">
+            {selectedCategories.map((category) => (
+               <span
+                  key={category}
+                  className="bg-blue-500 text-white px-2 py-1 rounded-full mr-2 mt-2 flex items-center"
+               >
+                  {category}
+                  <button
+                     onClick={() => handleRemoveCategory(category)}
+                     className="ml-3 mr-1 focus:outline-none text-lg"
+                  >
+                     x
+                  </button>
+               </span>
+            ))}
+         </div>
       </div>
-    </div>
-  );
-};
+   )
+}
 
-export default CategorySelector;
+export default CategorySelector
